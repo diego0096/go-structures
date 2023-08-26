118ee88290f83097c5595a0982a9664b4580cb11
Diego Felipe Quijano Zuñiga <dfqz93@hotmail.com>
2023-08-26 05:46:31 -0500

    prompt

---
+++ teclado/ingresos.go
@@ -0,0 +1,42 @@
+package teclado
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var numero1 int
+var numero2 int
+var leyenda string
+var err error
+
+func IngresoNumeros() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Ingresa número 1: ")
+	if scanner.Scan() {
+		numero1, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic("el dato ingreado es incorrecto"+err.Error())
+		}
+	}
+
+	fmt.Println("Ingresa número 2: ")
+	if scanner.Scan() {
+		numero2, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic("el dato ingreado es incorrecto"+err.Error())
+		}
+	}
+
+	fmt.Println("Ingresa leyenda: ")
+	if scanner.Scan() {
+		leyenda = scanner.Text()
+		if err != nil {
+			panic("el dato ingreado es incorrecto"+err.Error())
+		}
+	}
+
+	fmt.Println(leyenda, numero1*numero2)
+}
